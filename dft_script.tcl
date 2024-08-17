73131a985e5d565b136e349cafda8b7e4b9443f3
mohabtarek650 <133497827+mohabtarek650@users.noreply.github.com>
2024-08-17 09:34:38 -1000

    Add files via upload

---
+++ dft_script.tcl
@@ -0,0 +1,105 @@
+
+
+set top_module SYS_TOP_dft
+
+define_design_lib work -path ./work
+
+
+puts "###########################################"
+puts "#      #setting Design Libraries           #"
+puts "###########################################"
+
+#Add the path of the libraries to the search_path variable
+lappend search_path /home/IC/Labs/SYSTEM/std_cells
+lappend search_path /home/IC/Labs/SYSTEM/rtl
+
+set SSLIB "scmetro_tsmc_cl013g_rvt_ss_1p08v_125c.db"
+set TTLIB "scmetro_tsmc_cl013g_rvt_tt_1p2v_25c.db"
+set FFLIB "scmetro_tsmc_cl013g_rvt_ff_1p32v_m40c.db"
+
+## Standard Cell libraries 
+set target_library [list $SSLIB $TTLIB $FFLIB]
+
+## Standard Cell & Hard Macros libraries 
+set link_library [list * $SSLIB $TTLIB $FFLIB]  
+
+set_svf SYSTEM_TOP_dft.svf
+
+puts "###########################################"
+puts "#             Reading RTL Files           #"
+puts "###########################################"
+
+set file_format verilog
+
+analyze -format $file_format [glob  /home/IC/Labs/SYSTEM/rtl/*.v]
+
+elaborate -lib work SYS_TOP_dft
+
+current_design $top_module
+
+
+link
+
+
+
+group_path -name INREG -from [all_inputs]
+#group_path -name REGOUT -from [all_outputs]
+group_path -name INOUT -from [all_inputs] -to [all_outputs]
+
+
+source -echo ./cons.tcl
+
+compile 
+
+
+set_scan_configuration -clock_mixing no_mix  \
+                       -style multiplexed_flip_flop \
+                       -replace true -max_length 100  
+
+compile -scan
+
+
+set test_default_period 100
+set test_default_delay 0
+set test_default_bidir_delay 0
+set test_default_strobe 20
+set test_default_strobe_width 0
+
+
+set_dft_signal -port [get_ports scan_clk]  -type ScanClock   -view existing_dft  -timing {30 60}
+set_dft_signal -port [get_ports scan_rst]  -type Reset       -view existing_dft  -active_state 0
+set_dft_signal -port [get_ports test_mode] -type Constant    -view existing_dft  -active_state 1 
+set_dft_signal -port [get_ports test_mode] -type TestMode    -view spec          -active_state 1 
+set_dft_signal -port [get_ports SE]        -type ScanEnable  -view spec          -active_state 1   -usage scan
+set_dft_signal -port [get_ports SI]        -type ScanDataIn  -view spec 
+set_dft_signal -port [get_ports SO]        -type ScanDataOut -view spec  
+
+
+create_test_protocol
+
+
+dft_drc -verbose
+
+
+preview_dft -show scan_summary
+
+
+insert_dft
+compile -scan -incremental
+
+
+dft_drc -verbose -coverage_estimate
+
+
+write_file -format verilog -hierarchy -output SYSTEM_dft.v
+
+
+report_area -hierarchy > Area.rpt
+report_power -hierarchy > power.rpt
+report_timing -max_paths 100 -delay_type min > hold.rpt
+report_timing -max_paths 100 -delay_type max > setup.rpt
+report_clock -attributes > clocks.rpt
+report_constraint -all_violators > constraints.rpt
+
+#gui_start
+
